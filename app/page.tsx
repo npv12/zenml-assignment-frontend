8a4c22e39dd05154fc1c6ab1dde8885ca910f7b5
Pranav <npv12@iitbbs.ac.in>
2024-07-14 19:18:23 +0530

    feat: initial routing for stacks
    Signed-off-by: Pranav <npv12@iitbbs.ac.in>

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Stacks() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hey</div>
---
+++ app/stacks/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useParams } from "next/navigation";
+
+export default function StackPage() {
+  const { id } = useParams();
+  console.log(id);
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Index Page</div>
+    </main>
+  );
+}
